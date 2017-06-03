04baf51f4370fc826483c34082d53cdee59cb8da
Alexander Simko <asimko94@gmail.com>
2017-06-03 10:21:55 +0200

    refractored the project

--- objects/Movable.java
+++
@@ -1,6 +0,0 @@
-package objects;
-
-public interface Movable {
-	
-	public void mouseMove(int newX, int newY);
-}
--- objects/Pressable.java
+++
@@ -1,6 +0,0 @@
-package objects;
-
-public interface Pressable {
-	
-	public boolean mousePress(int x, int y);
-}
